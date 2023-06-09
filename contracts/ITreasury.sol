f170d878ffd05dacc806875d444ff53f85fa61d3
Pooh <87540521+yellowbearpooh0110@users.noreply.github.com>
2023-06-09 06:30:34 -0400

    my usd contract

--- contracts/ITreasury.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-interface ITreasury {
-    function getMyUSDUpdatedPrice() external view returns (uint256 _myUsdPrice);
-
-    function getMyUSDPrice() external view returns (uint256 myUsdPrice);
-}
